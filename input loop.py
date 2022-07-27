ae874cc55051d5295d5d90de3207dc752c11eefb
khanpranto <97869460+khanpranto@users.noreply.github.com>
2022-07-27 16:50:10 +0600

    Add files via upload

---
+++ input loop.py
@@ -0,0 +1,19 @@
+n = int(input())
+j = int(input())
+
+
+if n <= 10:
+    while n>=1:
+        print("pranto",end=" ")
+        n = n-1
+
+        while j>=1:
+            print("khan")
+            j = j-1
+            break
+
+else:
+    print("this is tooo biigg")
+
+
+
---
+++ while loop.py
@@ -0,0 +1,12 @@
+i = 1
+
+while i <= 5:
+    print("pranto",end=" ")
+    j = 1
+    i = i + 1
+    while j <=1:
+        print("khan",end=" ")
+        j = j+1
+
+
+    print()
